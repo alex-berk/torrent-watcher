63e6da65c652695e15ee67016be000cc7769cf9a
Alexander Berkovich <alex-berk@users.noreply.github.com>
2023-11-25 03:41:00 +0200

    🧪 adding tests for pb_client (#24)

--- tests/test_pb_client.py
+++ tests/test_pb_client.py
@@ -24,8 +24,7 @@ with open("tests/fixtures/pb_response.json", "r") as file:
     pb_response = PBResponse(data)
 
 
-@pytest.fixture
-def mock_response(monkeypatch: pytest.MonkeyPatch):
+def generate_mock_get(response_type: str = "") -> (callable, dict):
     buffer = {"url": "", "query": "", "calls": 0}
 
     def mock_get(url, **kwargs):
@@ -34,13 +33,32 @@ def mock_response(monkeypatch: pytest.MonkeyPatch):
         buffer["query"] = query
         buffer["calls"] += 1
 
-        if query == "query_with_no_results":
+        if response_type == "empty":
             return pb_response_no_results
-        elif query.startswith("throw"):
-            desired_status_code = int(query.split()[1])
-            return PBResponse(status_code=desired_status_code)
+        elif response_type == "404":
+            return PBResponse(status_code=404)
         return pb_response
 
+    return mock_get, buffer
+
+
+@pytest.fixture
+def mock_response(monkeypatch: pytest.MonkeyPatch):
+    mock_get, buffer = generate_mock_get()
+    monkeypatch.setattr(requests, "get", mock_get)
+    return buffer
+
+
+@pytest.fixture
+def mock_response_empty(monkeypatch: pytest.MonkeyPatch):
+    mock_get, buffer = generate_mock_get("empty")
+    monkeypatch.setattr(requests, "get", mock_get)
+    return buffer
+
+
+@pytest.fixture
+def mock_response_404(monkeypatch: pytest.MonkeyPatch):
+    mock_get, buffer = generate_mock_get("404")
     monkeypatch.setattr(requests, "get", mock_get)
     return buffer
 
@@ -55,21 +73,19 @@ def mock_timeout(monkeypatch: pytest.MonkeyPatch):
 class TestPBSearcher:
     def setup_method(self, method):
         self.searcher = PBSearcher("akira")
-        self.searcher_empty_results = PBSearcher("query_with_no_results")
+        self.searcher_empty_results = PBSearcher("akira")
 
     def test_search_torrent_sorting(self, mock_response):
         results = self.searcher.search_torrent()
         torrent_names = [result.name for result in results]
         assert torrent_names == ["Torrent 3", "Torrent 1", "Torrent 2"]
 
-    def test_search_torrent_no_results(self, mock_response):
-        results = self.searcher.search_torrent("query_with_no_results")
+    def test_search_torrent_no_results(self, mock_response_empty):
+        results = self.searcher.search_torrent("non existing")
         assert results == []
 
-    def test_search_torrent_http_error_handling(self, mock_response):
-        assert self.searcher.search_torrent("throw 404") == []
-        assert self.searcher.search_torrent("throw 500") == []
-        assert self.searcher.search_torrent("throw 301") == []
+    def test_search_torrent_http_error_handling(self, mock_response_404):
+        assert self.searcher.search_torrent("its not working") == []
 
     def test_search_torrent_http_timeout(self, mock_timeout):
         results = self.searcher.search_torrent()
@@ -79,10 +95,59 @@ class TestPBSearcher:
         results = self.searcher.look()
         assert results
 
-    def test_look_no_results(self, mock_response):
+    def test_look_no_results(self, mock_response_empty):
         results = self.searcher_empty_results.look()
         assert results == None
 
     def test_look_http_timeout(self, mock_timeout):
         results = self.searcher.look()
         assert results == None
+
+
+class TestPBMonitor:
+    def setup_method(self, method):
+        self.monitor = PBMonitor(
+            "attack on titan", episode_number=1, season_number=1)
+
+    def test_look(self, mock_response):
+        assert self.monitor.episode_number == 1
+        self.monitor.look()
+        assert self.monitor.episode_number == 2
+
+    def test_look_max_seeders(self, mock_response):
+        result = self.monitor.look()
+        assert result.name == "Torrent 3"
+
+    def test_look_no_episodes(self, mock_response_empty):
+        assert self.monitor.episode_number == 1
+        result = self.monitor.look()
+        assert result == None
+        assert self.monitor.episode_number == 1
+
+    def test_look_max_seeders_with_size_limit(self, mock_response):
+        self.monitor.size_limit_gb = 1
+        result = self.monitor.look()
+        assert result.name == "Torrent 1"
+
+    def test_look_max_seeders_with_small_size_limit(self, mock_response):
+        assert self.monitor.episode_number == 1
+        self.monitor.size_limit_gb = 0.5
+        result = self.monitor.look()
+        assert result == None
+        assert self.monitor.episode_number == 1
+
+    def test_look_http_error(self, mock_response_404):
+        self.monitor.look()
+        assert self.monitor.episode_number == 1
+
+    def test_look_timeout(self, mock_timeout):
+        self.monitor.look()
+        assert self.monitor.episode_number == 1
+
+    def test_look_zero_episode(self, mock_response):
+        self.monitor.episode_number = 0
+        self.monitor.season_number = 0
+        self.monitor.look()
+        assert mock_response["query"] == "attack on titan s00e00"
+        self.monitor.look()
+        assert mock_response["query"] == "attack on titan s00e01"
