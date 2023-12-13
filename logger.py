e51629cb063be697d93b5a45435bc62b4bedc3c0
Alex Berk <aleks.berkov@gmail.com>
2023-12-13 15:46:15 +0200

    added logs file handler

--- logger.py
+++ logger.py
@@ -1,3 +1,4 @@
+import sys
 from loguru import logger
 from logtail import LogtailHandler
 from config import LOGTAIL_TOKEN
@@ -5,10 +6,20 @@ from config import LOGTAIL_TOKEN
 
 logtail_handler = LogtailHandler(source_token=LOGTAIL_TOKEN)
 
+logger.remove(0)
+
+logger.add(sys.stdout, colorize=True, format="<green>{time}</green> <level>{message}</level>", level="INFO")
+
 logger.add(
     logtail_handler,
     format="{message}",
     level="INFO",
     backtrace=False,
     diagnose=False
+)
+
+logger.add(
+    "logs/{time:YYYY-MM-DD}.log",
+    rotation="1 day",
+    retention="1 month"
 )
