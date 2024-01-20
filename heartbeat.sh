0ecfcf287e90aca680467b921a23c32798c50906
Alex Berk <aleks.berkov@gmail.com>
2024-01-20 17:16:28 +0200

    move heartbeat inside the watch job

--- heartbeat.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-source ./health-check.sh
-if [ $? -eq 0 ]; then
-   curl "https://uptime.betterstack.com/api/v1/heartbeat/$HEARTBEAT_KEY"  
-fi
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from multiprocessing import Process
 from time import sleep
 from telegram.ext import ApplicationBuilder
 from telegram.error import NetworkError as TelegramNetworkError
-from config import TRANSMISSION_HOST, TG_BOT_TOKEN, ALLOWED_TG_IDS
+from config import TRANSMISSION_HOST, TG_BOT_TOKEN, ALLOWED_TG_IDS, HEARTBEAT_KEY
 from logger import logger
+from requests import get
 
 from torrent_manager import TransmissionClient, MonitorOrchestrator, PBSearcher
 from tg_bot import TgBotRunner
@@ -24,8 +25,13 @@ runner = TgBotRunner(tg_client=ApplicationBuilder().token(TG_BOT_TOKEN).build(),
                      tg_user_whitelist=users_whitelist)
 
 
+def emit_heartbeat():
+    get("https://uptime.betterstack.com/api/v1/heartbeat/" + HEARTBEAT_KEY)
+
+
 def run_search_jobs_on_timer(timer_seconds):
     logger.debug("run_search_jobs_on_timer running")
+    emit_heartbeat()
     runner.download_new_finds(admin_tg_id)
     sleep(timer_seconds)
     run_search_jobs_on_timer(timer_seconds)
