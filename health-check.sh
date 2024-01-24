6190811761edb037d6318a0e789685778895a2c6
Alex Berk <aleks.berkov@gmail.com>
2024-01-24 09:22:42 +0200

    small fixes

--- health-check.sh
+++ health-check.sh
@@ -8,14 +8,12 @@ LOG_FILE="$LOG_FOLDER/$(ls -t "$LOG_FOLDER" | head -n 1)"
 FRESHEST_LOG=$(tac $LOG_FILE | grep -m1 "$LOG_HEALTH_FLAG" | cut -d "|" -f 1)
 FRESHEST_LOG_DATE=$(date -d "$FRESHEST_LOG" +"%s")
 
-# Check that log was fetched
 if [ -z "$FRESHEST_LOG" ];
 then
         echo "healthcheck: couldn't fetch log timestamp"
         exit 1
 fi
 
-# Check that fetched log is younger then 8 hrs
 if [ "$TIME_THRESHOLD" -gt "$FRESHEST_LOG_DATE" ];
 then
         echo "healthcheck: freshest timestamp is too old"
