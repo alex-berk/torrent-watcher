860928317214413693bc14b759116e43b4bf16f2
Alex Berk <aleks.berkov@gmail.com>
2024-02-04 01:35:03 +0200

    remove logtail logging from dev env

--- logger.py
+++ logger.py
@@ -3,20 +3,9 @@ from loguru import logger
 from logtail import LogtailHandler
 from config import LOGTAIL_TOKEN
 
-
-logtail_handler = LogtailHandler(source_token=LOGTAIL_TOKEN)
-
 logger.remove(0)
 
 logger.add(sys.stdout, colorize=True, format="<level>{time}</level> <green>{message}</green>", level="INFO")
-
-logger.add(
-    logtail_handler,
-    format="{message}",
-    level="DEBUG",
-    backtrace=False,
-    diagnose=False
-)
 
 logger.add(
     "logs/{time:YYYY-MM-DD}.log",
@@ -24,3 +13,14 @@ logger.add(
     retention="1 month",
     enqueue=True
 )
+
+if LOGTAIL_TOKEN:
+    logtail_handler = LogtailHandler(source_token=LOGTAIL_TOKEN)
+
+    logger.add(
+        logtail_handler,
+        format="{message}",
+        level="DEBUG",
+        backtrace=False,
+        diagnose=False
+    )
